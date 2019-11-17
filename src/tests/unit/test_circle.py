8c0d4aa45159bd0bbc4d19cc31a7ee616388a211
Piotr Kopalko <copalco@gmail.com>
2019-11-17 22:07:11 +0100

    Add test that verifies that previous returns last area after first.

--- src/tests/unit/test_circle.py
+++ src/tests/unit/test_circle.py
@@ -34,3 +34,12 @@ class TestCircle(unittest.TestCase):
             last_area
         )
         self.assertEqual(circle.previous(last_area), first_area)
+
+    def test_previous_returns_last_area_if_first_is_given(self) -> None:
+        first_area = Area(Size(10, 10), Position(0, 0)),
+        last_area = Area(Size(10, 10), Position(10, 0)),
+        circle = Circle(
+            first_area,
+            last_area,
+        )
+        self.assertEqual(circle.previous(first_area), last_area)
