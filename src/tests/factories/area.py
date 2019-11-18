59f7ede4d02342e9533359c6acfabfb066e8f950
Piotr Kopalko <copalco@gmail.com>
2019-11-18 22:34:15 +0100

    Parametrize area factory.

--- src/tests/factories/area.py
+++ src/tests/factories/area.py
@@ -1,4 +1,6 @@
 from savetheprince.maparea import MapArea
+from savetheprince.position import Position
+from savetheprince.size import Size
 from tests.factories.area_id import AreaIdFactory
 from tests.factories.position import PositionFactory
 from tests.factories.size import SizeFactory
@@ -7,9 +9,11 @@ from tests.factories.size import SizeFactory
 class AreaFactory:
 
     @staticmethod
-    def create() -> MapArea:
+    def create(size: Size = None, position: Position = None) -> MapArea:
+        size = size or SizeFactory.create()
+        position = position or PositionFactory.create()
         return MapArea(
             id=AreaIdFactory.create(),
-            size=SizeFactory.create(),
-            position= PositionFactory.create(),
+            size=size,
+            position=position,
         )
