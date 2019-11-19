1e74b4c1534772ed993b3d1ce522f57dace63191
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-19 21:21:47 +0100

    Use make for running tests in tcr.sh

--- src/tcr.sh
+++ src/tcr.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-fswatch -e ".*" -i "\\.py$" . | xargs -n1 -o sh -c "python -m unittest discover tests && git add . && git commit -v || git checkout -- savetheprince/"
+fswatch -e ".*" -i "\\.py$" . | xargs -n1 -o sh -c "make test && git add . && git commit -v || git checkout -- savetheprince/"
