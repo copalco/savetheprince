adc97dd9d85b52021b924555323c580b8a1e2b27
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-19 21:31:53 +0100

    Run both tests and type-check via make in the TCR script

--- src/tcr.sh
+++ src/tcr.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-fswatch -e ".*" -i "\\.py$" . | xargs -n1 -o sh -c "make test && git add . && git commit -v || git checkout -- savetheprince/"
+fswatch -e ".*" -i "\\.py$" . | xargs -n1 -o sh -c "make ci-build && git add . && git commit -v || git checkout -- savetheprince/"
