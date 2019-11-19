06b0fc3e39e44cfe5d4e91243bebf6dd7ff179f2
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-19 21:58:23 +0100

    Use Size.half_height in the MapArea

--- src/savetheprince/maparea.py
+++ src/savetheprince/maparea.py
@@ -15,9 +15,9 @@ class MapArea:
 
     def centered(self, size: Size) -> Position:
         half_width = self._size.half_width()
-        half_height = self._size.height / 2.0
+        half_height = self._size.half_height()
         half_other_width = size.half_width()
-        half_other_height = size.height / 2.0
+        half_other_height = size.half_height()
         return Position(
             round(half_width - half_other_width) + self._position.x,
             round(half_height - half_other_height) + self._position.y,
