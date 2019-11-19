58cae8b58365e5d780b44232cf95e68a11e25108
Ignacy Sokolowski <ignacy.sokolowski@gmail.com>
2019-11-19 20:59:36 +0100

    Rename area tests

--- src/tests/unit/test_area.py
+++ src/tests/unit/test_area.py
@@ -8,11 +8,11 @@ from savetheprince.size import Size
 
 class TestArea(unittest.TestCase):
 
-    def test_center_returns_position_of_item_centered_inside(
-            self) -> None:
+    def test_finds_position_of_an_item_in_the_center(self) -> None:
         area = MapArea(AreaId('test-area'), Size(100, 100), Position(0, 0))
         self.assertEqual(area.centered(Size(10, 10)), Position(45, 45))
 
-    def test_center_returns_absolute_position(self) -> None:
+    def test_finds_absolute_position(self) -> None:
         area = MapArea(AreaId('test-area'), Size(100, 100), Position(15, 100))
         self.assertEqual(area.centered(Size(10, 10)), Position(60, 145))
+
