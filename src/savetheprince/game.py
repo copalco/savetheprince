57cb219e1264a646cd280740140ca4f40782e4c6
Piotr Kopalko <copalco@gmail.com>
2019-11-21 22:11:10 +0100

    Add hero to game class

--- src/savetheprince/game.py
+++ src/savetheprince/game.py
@@ -1,10 +1,12 @@
 from savetheprince.circle import Circle
+from savetheprince.hero import Hero
 
 
 class Game:
 
-    def __init__(self, circle: Circle) -> None:
+    def __init__(self, circle: Circle, hero: Hero) -> None:
         self._circle = circle
+        self._hero = hero
 
     def move_left(self) -> None:
         pass
--- src/tests/unit/test_game.py
+++ src/tests/unit/test_game.py
@@ -2,12 +2,14 @@ import unittest
 
 from savetheprince.circle import Circle
 from savetheprince.game import Game
+from savetheprince.hero import Hero
 from tests.factories.area_factory import AreaFactory
 
 
 class TestGame(unittest.TestCase):
 
-    def test_raises_movement_event(self) -> None:
+    def test_changes_hero_position(self) -> None:
+        hero = Hero()
         area_to_the_left = AreaFactory.create()
-        game = Game(Circle(AreaFactory.create(), area_to_the_left))
+        game = Game(Circle(AreaFactory.create(), area_to_the_left), hero)
         game.move_left()
