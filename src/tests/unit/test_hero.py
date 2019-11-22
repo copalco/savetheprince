fb127bcf0fb3a275a715724e13107485bd24680e
Piotr Kopalko <copalco@gmail.com>
2019-11-22 10:01:57 +0100

    Rename test

--- src/tests/unit/test_hero.py
+++ src/tests/unit/test_hero.py
@@ -6,6 +6,6 @@ from tests.factories.area_id import AreaIdFactory
 
 class TestHero(unittest.TestCase):
 
-    def test_is_aware_of_position(self) -> None:
+    def test_is_aware_of_location(self) -> None:
         area_id = AreaIdFactory.create()
         Hero(location=area_id)
