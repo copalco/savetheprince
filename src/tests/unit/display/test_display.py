f964447929bccfe21e85a310c88e570da3b7a7ea
Piotr Kopalko <copalco@gmail.com>
2020-01-19 16:13:56 +0100

    Inline sizes in scale ratio calculation tests

--- src/tests/unit/display/test_display.py
+++ src/tests/unit/display/test_display.py
@@ -8,9 +8,7 @@ from savetheprince.size import Size
 class TestDisplay(unittest.TestCase):
 
     def test_calculates_scale_ratio(self) -> None:
-        initial_size = Size(20, 10)
-        final_size = Size(10, 5)
         self.assertEqual(
-            calculate_scale_ratio(initial_size, final_size),
+            calculate_scale_ratio(Size(20, 10), Size(10, 5)),
             ScaleRatio(0.5, 0.5)
         )
