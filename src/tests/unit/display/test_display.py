8b908022dbd19e0b2b2b2aa83c0e66b9a789e1d7
Piotr Kopalko <copalco@gmail.com>
2020-01-21 00:24:02 +0100

    Change test to subtests

--- src/tests/unit/display/test_display.py
+++ src/tests/unit/display/test_display.py
@@ -8,15 +8,17 @@ from savetheprince.size import Size
 class TestDisplay(unittest.TestCase):
 
     def test_calculates_scale_ratio(self) -> None:
-        self.assertEqual(
-            calculate_scale_ratio(Size(20, 10), Size(10, 5)),
-            ScaleRatio(0.5, 0.5)
+        cases = (
+            (Size(20, 10), Size(10, 5), ScaleRatio(0.5, 0.5)),
+            (Size(30, 10), Size(10, 5), ScaleRatio(0.3333333333333333, 0.5)),
+            (Size(20, 4), Size(20, 15), ScaleRatio(1, 3.75)),
         )
-        self.assertEqual(
-            calculate_scale_ratio(Size(30, 10), Size(10, 5)),
-            ScaleRatio(0.3333333333333333, 0.5)
-        )
-        self.assertEqual(
-            calculate_scale_ratio(Size(20, 4), Size(20, 15)),
-            ScaleRatio(1, 3.75)
-        )
+        for initial_size, final_size, scale_ratio in cases:
+            with self.subTest(
+                    initial_size=initial_size,
+                    final_size=final_size,
+                    scale_ratio=scale_ratio):
+                self.assertEqual(
+                    calculate_scale_ratio(initial_size, final_size),
+                    scale_ratio,
+                )
